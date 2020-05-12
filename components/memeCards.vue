555f0124ee2690fe3b5371324d91efe498c80123
David Rashid <rashid.david12@gmail.com>
2020-05-12 23:54:07 +0300

    initial commit

---
+++ components/memeCards.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-bottom-navigation
+    :value="activeBtn"
+    grow
+    color="dark"
+    dark
+  >
+    <v-btn>
+      <span>Home</span>
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Favorites</span>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Nearby</span>
+      <v-icon>mdi-map-marker</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        activeBtn: 1,
+      }
+    },
+  }
+</script>
